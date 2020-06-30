20dce179b04d4f8bdabf9aa88f119c96a7f6b845
Gene Backlin <gbacklin@marizack.com>
2020-06-29 20:51:48 -0500

    Initial Commit

---
+++ TestCoreDataUI/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TestCoreDataUI
+//
+//  Created by Gene Backlin on 6/29/20.
+//  Copyright © 2020 Gene Backlin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
